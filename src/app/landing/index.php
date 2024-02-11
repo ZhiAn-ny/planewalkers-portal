ab42f7e4d9d041bf79b5d05ac29cf54348f61560
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2024-02-11 21:11:37 +0100

    Add landing page

---
+++ src/app/landing/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="landing.css" rel="stylesheet" type="text/css"/>
+    <link href="../../assets/css/global.css" rel="stylesheet" type="text/css"/>
+    <script src="https://kit.fontawesome.com/93710f8f6f.js" crossorigin="anonymous"></script>
+    <!--<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" type="text/javascript"></script> -->
+    <title>Planeswalker's Portal</title>
+</head>
+<body>
+    <main>
+        <section class="fx-col">
+            <article>
+                <p>Welcome to the</p>
+                <h1>Planeswalker's Portal</h1>
+                <p>
+                    Immerse yourself in a world of fantasy and create meaningful 
+                    adventures with your friends.
+                </p>
+            </article>
+            <button>
+                <a href="../auth/login.php">Cross the portal</a>
+            </button>
+        </section>
+    </main>
+</body>
+</html>
