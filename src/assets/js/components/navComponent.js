29b5f161a09a01856965e778ca50824f1f52621a
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2024-04-03 10:46:54 +0200

    Toggle notifications' div

--- src/assets/js/components/navComponent.js
+++ src/assets/js/components/navComponent.js
@@ -4,6 +4,7 @@ export class NavComponent extends HTMLElement {
     homeBtn;
     feedBtn;
     profBtn;
+    ntfPopup;
     #initalized = false;
 
     constructor() { super(); }
@@ -15,10 +16,12 @@ export class NavComponent extends HTMLElement {
             <button id="goto-feed-btn"><i class="fa-solid fa-envelopes-bulk fa-lg"></i></button>
             <button id="goto-profile-btn"><i class="fa-solid fa-circle-user fa-lg"></i></button>
         </nav>
+        <div id="notifications-popup" class="glassmorph"></div>
         `;
         if (!this.#initalized) {
             this.#initButtons();
             this.#setOnBtnPress();
+            this.#initPopUp();
             this.#initalized = true;
         }
     }
@@ -27,12 +30,34 @@ export class NavComponent extends HTMLElement {
         this.homeBtn = document.getElementById("goto-home-btn");
         this.feedBtn = document.getElementById("goto-feed-btn");
         this.profBtn = document.getElementById("goto-profile-btn");
+        this.ntfPopup = document.getElementById("notifications-popup");
+    }
+
+    #initPopUp() {
+        this.ntfPopup = document.getElementById("notifications-popup");
+        let style = "height: 200px; width: 70%;" 
+            + "background: var(--accent-t); position: absolute;"
+            + "top: 40px; left: 15%;"
+            + "border: 1px solid rgba(255, 255, 255, 0.18);"
+            + "border-radius: 10px;"
+            + "backdrop-filter: blur(5px);"
+            + "box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);";
+        this.ntfPopup.setAttribute("style", style);
+        this.ntfPopup.hidden = true;
+        this.feedBtn.popup = this.ntfPopup;
     }
 
     #setOnBtnPress() {
         this.homeBtn.addEventListener("click", function() { redirect(Pages.Home) });
-        this.feedBtn.addEventListener("click", function() { redirect(Pages.Feed) });
         this.profBtn.addEventListener("click", function() { redirect(Pages.Profile) });
+        this.feedBtn.addEventListener("click", this.togglePopUp );
+    }
+
+    /** Toggles the notifications' popup.
+     * In this function `this` references the feed button.
+     */
+    togglePopUp() {
+        this.popup.hidden = !this.popup.hidden;
     }
 
     
@@ -47,7 +72,6 @@ function redirect(pageId) {
         if (!response.ok) {
             throw new Error("Redirect failed");
         }
-        console.log('Redirect ok');
         return response.json();
     }).then(response => {
         window.location.href = response.url;
